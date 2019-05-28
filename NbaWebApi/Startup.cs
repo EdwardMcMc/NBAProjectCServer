ec7f3f6a19e175c4f90736a7418b4f50c4ef8a8b
Richard Sims <9633324@student.swin.edu.au>
2019-05-28 16:46:52 +1000

    Added service configuration of local db

--- NbaWebApi/Startup.cs
+++ NbaWebApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -33,8 +34,13 @@ namespace NbaWebApi
                       .AllowAnyMethod();
             }));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddDbContext<NbaDbContext>();
-            
+
+            // Production - uses live cloud database
+            //services.AddDbContext<NbaDbContext>();
+
+            // Development - uses local db
+            services.AddDbContext<NbaDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("NbaDbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
